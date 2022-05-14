3a1e714c7ad42b5f0ee23ab2543dbc0405e22877
mellow.xyz <89431994+mellowxyz@users.noreply.github.com>
2022-05-14 15:36:11 +0000

    enter command

--- commands/bot/fun/enter.js
+++ commands/bot/fun/enter.js
@@ -2,8 +2,14 @@ module.exports = {
 name: "enter",
 code:
 `
+$deleteCommand
 
+**$username** has entered the chat <:Dw_anime_lurking2:967808131000315974>
 
+$onlyForRoles[844137698519678977;908379849352175653;$getVar[cross] you don't have access to this command, Check <#974560781632946217>]
+
+$coolDown[10s;$getVar[cross] **you are on cooldown of 10 seconds.**{delete:5s}]
 `
   
 }
+
